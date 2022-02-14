7822a5c449ee22faa5b7747d33bdec84f7a8a500
Ana-Krstevska <krstevska.ana@protonmail.com>
2022-02-14 19:59:57 +0100

    Add project files.

---
+++ Integrirani_Sistemi_Proekt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Integrirani_Sistemi_Proekt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
