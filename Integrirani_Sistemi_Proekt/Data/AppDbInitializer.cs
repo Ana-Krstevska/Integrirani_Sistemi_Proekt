f8f0be3e07bc3050b4c7e0d76b30c983bedcfc9d
Ana-Krstevska <krstevska.ana@protonmail.com>
2022-02-15 14:53:29 +0100

    Connecting to database and adding migratitons.

---
+++ Integrirani_Sistemi_Proekt/Data/AppDbInitializer.cs
@@ -0,0 +1,73 @@
+﻿using Integrirani_Sistemi_Proekt.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Integrirani_Sistemi_Proekt.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                context.Database.EnsureCreated();
+
+                //Shop
+                if(!context.Shops.Any())
+                {
+                    context.Shops.AddRange(new List<Shop>()
+                    {
+                        new Shop()
+                        {
+                            Name = "Ramstore Mall",
+                            Logo = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.FkDn1L4Rsks08ec34FazMQHaDT%26pid%3DApi&f=1",
+                            Description = "Na ulica Sv. Kiril i Metodij"
+                        },
+
+                        new Shop()
+                        {
+                            Name = "Skopje City Mall",
+                            Logo = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JgLCXd28JJSHbvs9MU_BrgHaFj%26pid%3DApi&f=1",
+                            Description = "Na bulevar Partizanski Odredi"
+                        },
+
+                        new Shop()
+                        {
+                            Name = "East Gate Mall",
+                            Logo = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.H_kdWpSf4gE2MgR6Vq5DdAHaE8%26pid%3DApi&f=1",
+                            Description = "Na ulica Belastisa"
+                        }
+
+                    });
+                }
+                //Tags
+                if (!context.Tags.Any())
+                {
+
+                }
+                //Brands
+                if (!context.Brands.Any())
+                {
+
+                }
+                //Clothings
+                if (!context.Clothings.Any())
+                {
+
+                }
+                //Tags & Clothings
+                if (!context.Tag_Clothings.Any())
+                {
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Integrirani_Sistemi_Proekt/Migrations/20220215132229_Initial.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Integrirani_Sistemi_Proekt.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Brands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProfilePictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Brands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Logo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shops", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Season = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClothingPiece = table.Column<int>(type: "int", nullable: false),
+                    Fabric = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clothings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AmountXS = table.Column<int>(type: "int", nullable: false),
+                    AmountS = table.Column<int>(type: "int", nullable: false),
+                    AmountM = table.Column<int>(type: "int", nullable: false),
+                    AmountL = table.Column<int>(type: "int", nullable: false),
+                    AmountXL = table.Column<int>(type: "int", nullable: false),
+                    BrandId = table.Column<int>(type: "int", nullable: false),
+                    ShopId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clothings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clothings_Brands_BrandId",
+                        column: x => x.BrandId,
+                        principalTable: "Brands",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Clothings_Shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "Shops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tag_Clothings",
+                columns: table => new
+                {
+                    ClothingId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tag_Clothings", x => new { x.TagId, x.ClothingId });
+                    table.ForeignKey(
+                        name: "FK_Tag_Clothings_Clothings_ClothingId",
+                        column: x => x.ClothingId,
+                        principalTable: "Clothings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tag_Clothings_Tags_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clothings_BrandId",
+                table: "Clothings",
+                column: "BrandId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clothings_ShopId",
+                table: "Clothings",
+                column: "ShopId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tag_Clothings_ClothingId",
+                table: "Tag_Clothings",
+                column: "ClothingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tag_Clothings");
+
+            migrationBuilder.DropTable(
+                name: "Clothings");
+
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropTable(
+                name: "Brands");
+
+            migrationBuilder.DropTable(
+                name: "Shops");
+        }
+    }
+}
